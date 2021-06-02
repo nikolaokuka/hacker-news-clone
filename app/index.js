3d59a80184cb0e269f546676ff444c472ac4f31b
Nikola Okuka <nikolaokuka@gmail.com>
2021-06-02 22:23:26 +0200

    init commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+
+export default function App() {
+  return (
+    <h1>
+      Hacker News Clone
+    </h1>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+}
